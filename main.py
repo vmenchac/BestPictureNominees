8a93214d5fee4624a8d610c7d2a829d7f6079b4e
vmenchac <71139140+vmenchac@users.noreply.github.com>
2021-03-23 17:59:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -18,17 +18,16 @@ For each movie, obtain:
 Create a pretty table and store the output in a file called output.txt
 Post the source code and the output file in your github.
 Share the link with sarifern@cisco.com
-The first submission of each category (begineer, intermediate, advanced) will get a price. 
+The first submission of each category (begineer, intermediate, advanced) will get a price.
 """
 from imdb import IMDb
 from prettytable import PrettyTable
-from prettytable import from_csv
 
 table = PrettyTable()
 table.field_names=["Tittle","Year","Rating","Director","Plot","Cast (Top 5)","Genre"]
 
 ia = IMDb()
-movie_list = ["Minari","Sound of Metal","Mank","Promising Young Woman","The Father","Judas and the Black Messiah","The Trial of the Chigago 7","Nomadland"]
+movie_list = [Minari","Sound of Metal","Mank","Promising Young Woman","The Father","Judas and the Black Messiah","The Trial of the Chigago 7","Nomadland"]
 
 file = open("movies.txt","w")
 
@@ -39,14 +38,19 @@ for movie in movie_list:
     title = movie["title"]
     year = movie["year"]
     rating = movie["rating"]
-    director = movie["director"][0]
+    #director = movie["director"][0]["name"]
+    directors = movie["director"]
+    list_directors = []
+    for i in range(len(directors)):
+        list_directors.append(directors[i]["name"])
     plot = movie["plot"][0]
     list_cast = []
     for cast in movie["cast"][:5]:
         list_cast.append(cast["name"])
     genre = movie["genre"]
 
-    table.add_row([str(title),str(year),str(rating),str(director),str(plot),str(list_cast),str(genre)])
+    #table.add_row([str(title),str(year),str(rating),str(director),str(plot),str(list_cast),str(genre)])
+    table.add_row([title,year,rating,list_directors,plot,list_cast,genre])
 
 file.writelines(table.get_string())
 file.close()
